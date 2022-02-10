8ac8409147fb5c92346e839da7543033a2666c99
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-02-10 14:11:27 +0530

    added bullet shooting events

--- main.py
+++ main.py
@@ -100,12 +100,12 @@ class Bullet:
         angle = math.atan2(mouseY - self.y, mouseX - self.x)
         self.Xvel = math.cos(angle) * 10
         self.Yvel = math.sin(angle) * 10
-    
-    
+
     def update(self):
         self.x += self.Xvel
         self.y += self.Yvel
         pygame.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), 5)
+
 
 def render_players(player, enemies):
     _player = Player(player.split("||")[1].split(":")[1])
@@ -153,18 +153,29 @@ def render_map():
         x = 0
         y += 32
 
+
 allBullets = []
+allUsers = net.send("message:get_all")
+allUsers = json.loads(allUsers)
+user = net.send("message:get")
+player, enemies = render_players(user, allUsers)
+
 
 while running:
+    # global allUsers, user
     clock.tick(120)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     render_map()
     user = net.send("message:get")
-    allUsers = net.send("message:get_all")
-    allUsers = json.loads(allUsers)
-    player, enemies = render_players(user, allUsers)
+    try:
+        allUsers = net.send("message:get_all")
+        allUsers = json.loads(allUsers)
+        player, enemies = render_players(user, allUsers)
+    except Exception as e:
+        # pass
+        print(e)
     player.rotation = get_rotation([player.x, player.y])
     net.send(f"message:update||{player.x}:{player.y}:{player.health}:{player.rotation}")
 
@@ -187,10 +198,12 @@ while running:
         )
     # if mouse left click
     if pygame.mouse.get_pressed()[0]:
-        # net.send(f"message:shoot||{player.x}:{player.y}")
-        allBullets.append(Bullet(player.x, player.y, player.id))
+        net.send(f"message:shoot||{player.x}:{player.y}")
+        # allBullets.append(Bullet(player.x, player.y, player.id))
     print(len(allBullets))
     for bullet in allBullets:
+        if bullet.x > 800 or bullet.x < 0 or bullet.y > 616 or bullet.y < 0:
+            allBullets.remove(bullet)
         bullet.update()
 
     pygame.display.flip()
