fc0d9f7b543befaf3a167fd9bff20ae9023d31a7
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-02-10 22:45:40 +0530

    different file for object classes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import network
 import json
 import math
+from objects import Player, Enemy, Bullet
 
 pygame.init()
 pygame.mixer.init()
@@ -13,100 +14,6 @@ net = network.Network()
 mapdata = net.send("message:get_map")
 
 
-class Player:
-    def __init__(self, id):
-        self.id = id
-        self.x = 0
-        self.y = 0
-        self.health = 100
-        self.rotation = 0
-
-    def update(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-        self.sprite = pygame.image.load("assets/player.png")
-        self.player = pygame.transform.rotate(self.sprite, self.rotation)
-
-        self.rect = self.player.get_rect()
-        self.rect.x = self.x - int(self.player.get_width() / 2)
-        self.rect.y = int(self.y - self.player.get_height() / 2)
-        self.hitbox = pygame.Rect(
-            self.rect.x, self.rect.y, self.player.get_width(), self.player.get_height()
-        )
-        screen.blit(self.player, self.hitbox)
-
-        self.playerData = f"{self.id} [{self.health}]"
-        self.playerText = pygame.font.SysFont("comicsans", 20).render(
-            self.playerData, 1, (255, 255, 255)
-        )
-        self.playerTextRect = self.playerText.get_rect()
-        self.playerTextRect.center = (self.x, self.y)
-        self.playerTextRect.x -= int(self.playerText.get_width() / 12)
-        self.playerTextRect.y -= int(self.playerText.get_height())
-        self.DataHitbox = pygame.Rect(
-            self.playerTextRect.x,
-            self.playerTextRect.y,
-            self.playerText.get_width(),
-            self.playerText.get_height(),
-        )
-        screen.blit(self.playerText, self.playerTextRect)
-
-
-class Enemy:
-    def __init__(self, id):
-        self.id = id
-        self.x = 0
-        self.y = 0
-        self.health = 100
-        self.rotation = 0
-
-    def update(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-        self.sprite = pygame.image.load("assets/enemy.png")
-        self.player = pygame.transform.rotate(self.sprite, self.rotation)
-
-        self.rect = self.player.get_rect()
-        self.rect.x = self.x - int(self.player.get_width() / 2)
-        self.rect.y = int(self.y - self.player.get_height() / 2)
-        self.hitbox = pygame.Rect(
-            self.rect.x, self.rect.y, self.player.get_width(), self.player.get_height()
-        )
-        screen.blit(self.player, self.hitbox)
-
-        self.playerData = f"{self.id} [{self.health}]"
-        self.playerText = pygame.font.SysFont("comicsans", 20).render(
-            self.playerData, 1, (255, 255, 255)
-        )
-        self.playerTextRect = self.playerText.get_rect()
-        self.playerTextRect.center = (self.x, self.y)
-        self.playerTextRect.x -= int(self.playerText.get_width() / 12)
-        self.playerTextRect.y -= int(self.playerText.get_height())
-        self.DataHitbox = pygame.Rect(
-            self.playerTextRect.x,
-            self.playerTextRect.y,
-            self.playerText.get_width(),
-            self.playerText.get_height(),
-        )
-        screen.blit(self.playerText, self.playerTextRect)
-
-
-class Bullet:
-    def __init__(self, x, y, by):
-        self.x = x
-        self.y = y
-        self.by = by
-        mouseX, mouseY = pygame.mouse.get_pos()
-        angle = math.atan2(mouseY - self.y, mouseX - self.x)
-        self.Xvel = math.cos(angle) * 10
-        self.Yvel = math.sin(angle) * 10
-
-    def update(self):
-        self.x += self.Xvel
-        self.y += self.Yvel
-        pygame.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), 5)
-
-
 def render_players(player, enemies):
     _player = Player(player.split("||")[1].split(":")[1])
     vals = {}
@@ -115,6 +22,7 @@ def render_players(player, enemies):
             vals[key.split(":")[0]] = int(key.split(":")[1])
         except:
             vals[key.split(":")[0]] = key.split(":")[1]
+    vals['screen'] = screen
     _player.update(**vals)
 
     _enemies = []
@@ -127,6 +35,7 @@ def render_players(player, enemies):
                     _val[key.split(":")[0]] = int(key.split(":")[1])
                 except:
                     _val[key.split(":")[0]] = key.split(":")[1]
+            _val['screen'] = screen
             _enemies[-1].update(**_val)
     return _player, _enemies
 
---
+++ objects.py
@@ -0,0 +1,96 @@
+import math, pygame
+
+class Player:
+    def __init__(self, id):
+        self.id = id
+        self.x = 0
+        self.y = 0
+        self.health = 100
+        self.rotation = 0
+        self.screen = None
+
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.sprite = pygame.image.load("assets/player.png")
+        self.player = pygame.transform.rotate(self.sprite, self.rotation)
+
+        self.rect = self.player.get_rect()
+        self.rect.x = self.x - int(self.player.get_width() / 2)
+        self.rect.y = int(self.y - self.player.get_height() / 2)
+        self.hitbox = pygame.Rect(
+            self.rect.x, self.rect.y, self.player.get_width(), self.player.get_height()
+        )
+        self.screen.blit(self.player, self.hitbox)
+
+        self.playerData = f"{self.id} [{self.health}]"
+        self.playerText = pygame.font.SysFont("comicsans", 20).render(
+            self.playerData, 1, (255, 255, 255)
+        )
+        self.playerTextRect = self.playerText.get_rect()
+        self.playerTextRect.center = (self.x, self.y)
+        self.playerTextRect.x -= int(self.playerText.get_width() / 12)
+        self.playerTextRect.y -= int(self.playerText.get_height())
+        self.DataHitbox = pygame.Rect(
+            self.playerTextRect.x,
+            self.playerTextRect.y,
+            self.playerText.get_width(),
+            self.playerText.get_height(),
+        )
+        self.screen.blit(self.playerText, self.playerTextRect)
+
+
+class Enemy:
+    def __init__(self, id):
+        self.id = id
+        self.x = 0
+        self.y = 0
+        self.health = 100
+        self.rotation = 0
+
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.sprite = pygame.image.load("assets/enemy.png")
+        self.player = pygame.transform.rotate(self.sprite, self.rotation)
+
+        self.rect = self.player.get_rect()
+        self.rect.x = self.x - int(self.player.get_width() / 2)
+        self.rect.y = int(self.y - self.player.get_height() / 2)
+        self.hitbox = pygame.Rect(
+            self.rect.x, self.rect.y, self.player.get_width(), self.player.get_height()
+        )
+        self.screen.blit(self.player, self.hitbox)
+
+        self.playerData = f"{self.id} [{self.health}]"
+        self.playerText = pygame.font.SysFont("comicsans", 20).render(
+            self.playerData, 1, (255, 255, 255)
+        )
+        self.playerTextRect = self.playerText.get_rect()
+        self.playerTextRect.center = (self.x, self.y)
+        self.playerTextRect.x -= int(self.playerText.get_width() / 12)
+        self.playerTextRect.y -= int(self.playerText.get_height())
+        self.DataHitbox = pygame.Rect(
+            self.playerTextRect.x,
+            self.playerTextRect.y,
+            self.playerText.get_width(),
+            self.playerText.get_height(),
+        )
+        self.screen.blit(self.playerText, self.playerTextRect)
+
+
+class Bullet:
+    def __init__(self, x, y, by):
+        self.x = x
+        self.y = y
+        self.by = by
+        mouseX, mouseY = pygame.mouse.get_pos()
+        angle = math.atan2(mouseY - self.y, mouseX - self.x)
+        self.Xvel = math.cos(angle) * 10
+        self.Yvel = math.sin(angle) * 10
+
+    def update(self):
+        self.x += self.Xvel
+        self.y += self.Yvel
+        pygame.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), 5)
+
