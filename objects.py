90a7ef5998a9baa0e482eac6e8170b87a990ae7e
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-04-16 17:02:00 +0530

    re written player class

--- objects.py
+++ objects.py
@@ -16,19 +16,21 @@ class Player:
         self.player = pygame.transform.rotate(self.sprite, self.rotation)
 
         self.rect = self.player.get_rect()
-        self.rect.x = self.x - int(self.player.get_width() / 2)
-        self.rect.y = int(self.y - self.player.get_height() / 2)
+        self.rect.x = self.pos[0] - int(self.player.get_width() / 2)
+        self.rect.y = int(self.pos[1] - self.player.get_height() / 2)
+
         self.hitbox = pygame.Rect(
             self.rect.x, self.rect.y, self.player.get_width(), self.player.get_height()
         )
         self.screen.blit(self.player, self.hitbox)
 
-        self.playerData = f"{self.id} [{self.health}]"
+        self.playerData = f"X: {self.pos[0]}, Y: {self.pos[1]}"
         self.playerText = pygame.font.SysFont("comicsans", 20).render(
             self.playerData, 1, (255, 255, 255)
         )
+
         self.playerTextRect = self.playerText.get_rect()
-        self.playerTextRect.center = (self.x, self.y)
+        self.playerTextRect.center = (self.pos[0], self.pos[1])
         self.playerTextRect.x -= int(self.playerText.get_width() / 12)
         self.playerTextRect.y -= int(self.playerText.get_height())
         self.DataHitbox = pygame.Rect(
